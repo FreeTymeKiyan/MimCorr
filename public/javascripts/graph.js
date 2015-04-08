af172d6db31c9bef9fa676beb8158f4cdd5be42f
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-04-08 11:24:16 -0400

    Added customization

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,34 +1,47 @@
 var width = 960,
     height = 500;
 
-var color = d3.scale.category20();
+var color = d3.scale.category20(); // 20 categorical colors
 
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
+var force = d3.layout.force() // position linked nodes, physical simulation
+    .charge(-120) // get or set the charge strength.
+    .linkDistance(100) // set the link distance
     .size([width, height]);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("body").append("svg") // set svg
     .attr("width", width)
     .attr("height", height);
 
-d3.json("../data/miserables.json", function(error, graph) {
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
+d3.json("../data/miserables.json", function(error, graph) { // add data 
+  force 
+      .nodes(graph.nodes) // set the array of nodes to layout
+      .links(graph.links) // set the array of links between nodes
       .start();
 
-  var link = svg.selectAll(".link")
+  var link = svg.selectAll(".link") // draw link
       .data(graph.links)
     .enter().append("line")
       .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+      .style("stroke-dasharray", function (d) { // set dashed-line
+        if (d.value % 3 == 0) return ("2, 2");
+        else if (d.value % 3 == 1) return ("10, 10");
+      });
 
-  var node = svg.selectAll(".node")
+  var node = svg.selectAll(".node") // draw node
       .data(graph.nodes)
-    .enter().append("circle")
+    .enter().append("path") // set shape
       .attr("class", "node")
-      .attr("r", 5)
+      .attr("d", d3.svg.symbol()
+        .type(function(d) { // set shape type
+          if (d.group % 2) return d3.svg.symbolTypes[0];
+          return d3.svg.symbolTypes[3]; 
+        })
+        .size(function(d) { // set shape size
+          return (d.group + 5) * (d.group + 5)
+        })
+      )
+      // .attr("r", 5)
       .style("fill", function(d) { return color(d.group); })
       .call(force.drag); // add drag
 
@@ -43,5 +56,9 @@ d3.json("../data/miserables.json", function(error, graph) {
 
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
+        
+    node.attr("transform", function(d) { // transform shape paths
+        return "translate(" + d.x + "," + d.y + ")";
+    });
   });
 });
