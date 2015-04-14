d9f13a409bb9c42373922ef65e26fef075f80ca8
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-04-14 11:47:10 -0400

    Added queryCorr

--- mysql.js
+++ mysql.js
@@ -11,27 +11,65 @@ var connection = mysql.createConnection({
 connection.connect();
 
 var testUsrInputs = ["EIF4B", "hsa-miR-1304", "HEATR1", "OR5M8", "hsa-miR-659", "hsa-miR-133a-2"];
-var testInput = "EIF4B";
-var values = [];
-var nodes = [];
-var middle = [];
+var testInput     = "EIF4B";
+var nodes         = [];
+var middle        = [];
+var res = { nodes : [] };
 
 var isMiRna = function (name) {
   return name.indexOf("hsa") === 0;
 };
 
-var queryCorr = function (input) {
-  var sql = "SELECT mrna, mirna, corr, genes_db_num FROM correlation WHERE " + (isMiRna(input) ? "mirna = ?" : "mrna = ?") + " AND genes_db_num != 0 AND corr != 1000000 ORDER BY corr ASC";
-  console.log(sql);
+var queryCorr = function (input, cb) {
+  var temp = isMiRna(input);
+  var sql = "SELECT mrna, mirna, corr, genes_db_num FROM correlation WHERE " 
+      + (temp ? "mirna = ?" : "mrna = ?") 
+      + " AND genes_db_num != 0 AND corr != 1000000 ORDER BY corr ASC";
+  // console.log(sql);
   var values = [];
   values.push(input);
-  connection.query(sql, values, function (err, rows, fields) {
-    console.log(rows);
+  connection.query(sql, values, function (err, rows, fields, cb) {
+    // cb(null, rows, temp);
+    cb(null, rows);
+    _.forEach(rows, function (val, key) {
+      queryCorr(val, null);
+    });
+    return;
   });
 }
 
-queryCorr(testInput);
+queryCorr(testInput, function (err, rows) {
+  
+});
 
+// function asyncLoop(iterations, func, callback) {
+//   var index = 0;
+//   var done = false;
+//   var loop = {
+//     next: function() {
+//       if (done) return;
+//       if (index < iterations) {
+//         index++;
+//         func(loop);
+//       } else {
+//         done = true;
+//         callback();
+//       }
+//     },
+//
+//     iteration: function() {
+//       return index - 1;
+//     },
+//
+//     break: function() {
+//       done = true;
+//       callback();
+//     }
+//
+//   };
+//   loop.next();
+//   return loop;
+// }
 
 // _.forEach(testUsrInputs, function (val, key) {
 //   var sql = "SELECT mrna, mirna, corr, genes_db_num FROM correlation WHERE " + (isMiRna(val) ? "mirna = ?" : "mrna = ?") + " AND genes_db_num != 0 AND corr != 1000000 ORDER BY corr ASC";
