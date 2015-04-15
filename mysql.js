315f077e3b89166206b7cad009874c9b64cdce62
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-04-14 23:31:18 -0400

    Added dfs

--- mysql.js
+++ mysql.js
@@ -1,5 +1,6 @@
 var _     = require("lodash");
 var mysql = require("mysql");
+var async = require("async");
 
 var connection = mysql.createConnection({
   host     : "localhost",
@@ -24,31 +25,41 @@ function getSql(isMiRna) {
     + " AND genes_db_num != 0 AND corr != 1000000 ORDER BY corr ASC";
 }
 
-var temp = isMiRna(testInput);
-var sql = getSql(temp);
-var values = [];
-values.push(input);
-
-connection.query(sql, values, function (err, rows, fields) {
-  
-  
-});
-
-function processRow(row, is, callback) {
-  var sql = getSql(is);
+function getAjacent(row, isMiRna, cb) {
+  var input = isMiRna ? row.mrna : row.mirna;
+  var sql = getSql(isMiRna);
   var values = [];
-  values.push((is ? row.mrna : row.mirna));=
+  values.push(input);
+
   connection.query(sql, values, function (err, rows, fields) {
-    async.each(rows, processRow, function (err) {
-      if (err) console.log(err);
-    });
+    if (err) cb(err);
+    else {
+      cb(null, rows);
+    }
   });
 }
 
+var middle = [];
+function search(row, isMiRna) {
+  if (!row) return;
+  middle.push(row);
+  console.log(middle.length);
+  getAjacent(row, isMiRna, function (err, rows) {
+    _.forEach(rows, function (val) {
+      if (!_.includes(res, row)) search(row, !isMiRna);
+    })
+  });
+}
+
+var root = {};
+root.mrna = testInput;
+search(root, false);
+
 function getNextRows(rows, isMiRna, callback1) {
   var nextRows = [];
+
   async.each(rows, function (row, callback2) {
-    var input = isMiRna ? row.mrna : row.mirna;
+    var input = isMiRna ? row.mirna : row.mrna;
     var sql = getSql(isMiRna);
     var values = [];
     values.push(input);
@@ -57,102 +68,17 @@ function getNextRows(rows, isMiRna, callback1) {
         callback2(err);
         return;
       }
-      callback2(null, rows);
+      nextRows = nextRows.concat(rows);
+      console.log(nextRows.length);
+      callback2();
     });
-  }, function (err, rows) {
-    console.log(callback2);
+  }, function (err) {
     if (err) {
       console.log(err);
       return;
     }
-    nextRows = nextRows.concat(rows);
     callback1(null, nextRows);
   });
 }
 
-
-// ====
-
-// var diff   = [];
-// var middle = [];
-//
-// var t = {
-//   mrna : testInput
-// };
-// diff.push(t);
-//
-
-//
-// var queryCorr = function (input, cb) {
-//   var temp = isMiRna(input);
-//   var sql = getSql(temp);
-//   // console.log(sql);
-//   var values = [];
-//   values.push(input);
-//   connection.query(sql, values, function (err, rows, fields) {
-//     diff = _.difference(rows, middle);
-//     middle = middle.concat(diff);
-//   });
-// };
-var queryCorrWithRow = function (row, isMiRna, cb) {
-  var input = isMiRna ? row.mirna : row.mrna;
-  var sql = getSql(isMiRna);
-  // console.log(sql);
-  var values = [];
-  values.push(input);
-  connection.query(sql, values, function (err, rows, fields) {
-    if (err) {
-      cb(err);
-    }
-    // get more rows from rows
-    var diff = _.difference(rows, middle);
-    if (diff.length === 0) {
-      cb(null, diff);
-    }
-  });
-};
-
-var callback = function (err, res) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  return res;
-}
-
-var test = function () { // continue when true
-  return diff.length !== 0; // diff len of middle and rows is not 0
-};
-
-async.whilst(
-  test,
-  queryCorrWithRow(), // query diff for rows
-  callback
-);
-// async.whilst(
-//   test,
-//   aysnc.each(diff, queryCorr, callback), // query diff for rows
-//   callback
-// );
-//
-// queryCorr(testInput, callback);
-//
 // connection.end();
-
-async = require("async");
-  
-async.each(
-  rows,
-  function(item, callback){
-    // Call an asynchronous function, often a save() to DB
-    item.someAsyncCall(function (){
-      // Async call is done, alert via callback
-      callback();
-    });
-  },
-  // 3rd param is the function to call when everything's done
-  function(err){
-    // All tasks are done now
-    doSomethingOnceAllAreDone();
-  }
-);
