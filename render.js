578d5e83ef2660fa84213a5a3a08d8664178722d
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-04-23 12:33:38 -0400

    Generated json data

--- render.js
+++ render.js
@@ -7,7 +7,7 @@ var async = require("async");
 const TYPE_MIRNA = 1;
 const TYPE_MRNA = 0 ;
 
-var res = { nodes : [], links : [] };
+var data = { "nodes" : [], "links" : [] };
 
 var read = function (callback, type) {
   var filename = (type === 0 ? "mirna.csv" : "mrna.csv");
@@ -30,7 +30,7 @@ async.parallel([
     read(callback, TYPE_MIRNA)
   },
   function (callback) {
-    fs.readFile("/Users/yongshengbai/Downloads/Data/MimCor/refined_30000.csv", "utf8", function (err, d) {
+    fs.readFile("/Users/yongshengbai/Downloads/Data/MimCor/refined_10000.csv", "utf8", function (err, d) {
       if (err) throw err;
       var res = d3.csv.parse(d);
       // console.log(res[0]);
@@ -39,9 +39,65 @@ async.parallel([
   }
 ], function (err, res) {
   if (err) throw err;
-  res.nodes = _.union(res[0], res[1]);
-  res.links = res[2];
+  data.nodes = _.union(res[0], res[1]);
+  data.links = res[2];
   // console.log(res.nodes[0]);
-  // console.log(res.links[0]);
-  // nodes got!
+  // console.log(res.links);
+  fs.writeFile("/Users/yongshengbai/Downloads/Data/MimCor/graph.json", JSON.stringify(data, null, 2), function () {
+    console.log("Data saved to file");
+  });
+  // TODO start to render graph
+  // renderGraph(res);
 });
+
+var renderGraph = function (links) {
+  var width = 960;
+  var height = 500;
+
+  var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  var force = d3.layout.force()
+    .size([width, height]);
+
+  var nodesByName = {};
+
+  links.forEach(function (link) {
+    link.source = nodeByName(link.source);
+    link.target = nodeByName(link.target);
+  });
+
+  var nodes = res.nodes;
+  var link = svg.selectAll(".link")
+      .data(links)
+    .enter().append("line")
+      .attr("class", "link");
+
+  var node = svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 4.5)
+      .call(force.drag);
+      
+  force
+      .nodes(nodes)
+      .links(links)
+      .on("tick", tick)
+      .start();
+  
+  function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+  
+  function nodeByName(name) {
+    return nodesByName[name] || (nodesByName[name] = {name: name});
+  }
+};
